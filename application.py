9d8fa9e3e8ab1232e344b53cfe9afd4bf66b5705
Syed zain ali shah <zaingit780@gmail.com>
2024-11-01 11:33:25 +0500

    create flask project and render html page

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name = request.args.get("name", "world")
+    return render_template("index.html", placeholder=name)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
